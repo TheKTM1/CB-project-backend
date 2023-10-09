c79ed582bf66b18c24b3ad164d7b6b5ec210f070
Konrad Krupa <34065758+TheKTM1@users.noreply.github.com>
2023-10-09 13:41:27 +0200

    Connected to SQLite database and created a repository for the users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as bcrypt from 'bcrypt';
 
-@Controller()
+@Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {  //rename to UserService
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  }
+
+  @Post('register')
+  async register(
+    @Body('name') name: string,
+    @Body('password') password: string,
+    ){
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      return this.appService.create({
+        name,
+        password: hashedPassword
+      });
+  }
+
+  @Post('login')
+  async login(
+    @Body('name') name: string,
+    @Body('password') password: string,
+  ){
+    const user = await this.appService.findOne({where: {name}});
+
+    if(!user){
+      throw new BadRequestException('No user with given name has been found.');
+    }
+
+    if(!await bcrypt.compare(password, user.password)){
+      throw new BadRequestException(`Given password does not match the user's password.`);
+    }
+
+    return user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './Entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database/cb-db.db',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
