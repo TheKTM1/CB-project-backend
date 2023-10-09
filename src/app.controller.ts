19a97e2c19f44004f3397ab28775565b2fd5b8b2
Konrad Krupa <34065758+TheKTM1@users.noreply.github.com>
2023-10-09 15:26:12 +0200

    Added JWT token authorization

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, BadRequestException, UnauthorizedException, Res, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtService } from '@nestjs/jwt';
+import { Response, Request } from 'express';
 import * as bcrypt from 'bcrypt';
 
 @Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {  //rename to UserService
+  constructor(
+    private readonly appService: AppService, //rename to UserService
+    private jwtService: JwtService
+  ) {
 
   }
 
@@ -15,16 +20,21 @@ export class AppController {
     ){
       const hashedPassword = await bcrypt.hash(password, 10);
 
-      return this.appService.create({
+      const user = await this.appService.create({
         name,
         password: hashedPassword
       });
+
+      delete user.password;
+
+      return user;
   }
 
   @Post('login')
   async login(
     @Body('name') name: string,
     @Body('password') password: string,
+    @Res({passthrough: true}) response: Response
   ){
     const user = await this.appService.findOne({where: {name}});
 
@@ -36,6 +46,43 @@ export class AppController {
       throw new BadRequestException(`Given password does not match the user's password.`);
     }
 
-    return user;
+    const jwt = await this.jwtService.signAsync({id: user.id});
+
+    response.cookie('jwt', jwt, {httpOnly: true});
+
+    return {
+      message: 'success'
+    };
+  }
+
+  @Get('user')
+  async user(@Req() request: Request){
+    try{
+      const cookie = request.cookies['jwt'];
+
+      const data = await this.jwtService.verifyAsync(cookie);
+
+      if(!data){
+        throw new UnauthorizedException();
+      }
+
+      const user = await this.appService.findOne({id: data['id']});
+
+      const {password, ...result} = user;
+
+      return result;
+      
+    } catch (e){
+      throw new UnauthorizedException();
+    }
+  }
+
+  @Post('logout')
+  async logout(@Res({passthrough: true}) response: Response){
+    response.clearCookie('jwt');
+
+    return {
+      message: 'success'
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt/dist';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './Entities/user.entity';
 
@@ -12,7 +13,11 @@ import { User } from './Entities/user.entity';
       entities: [User],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: {expiresIn: '1d'}
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
